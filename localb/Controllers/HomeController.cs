0f5df8eda38e407fe14e6ea5ff81ba39bc6dc243
Cusanelly <cuentacus@gmail.com>
2018-11-06 09:32:01 -0400

    LlamadosAPI
    - Se agrego url de prueba para verificar los llamados via API desde el controlador.
    - Se modifico archivo gitignore.

--- localb/Controllers/HomeController.cs
+++ localb/Controllers/HomeController.cs
@@ -5,14 +5,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using localb.Models;
+using System.Security.Cryptography;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace localb.Controllers
 {
     public class HomeController : Controller
-    {
+    {       
+        const string URL = "https://localbitcoins.com";
+        const string URLENDPOINT = "/buy-bitcoins-online";
         public IActionResult Index()
         {
-            return View();
+            HMACSHA256 hash = new HMACSHA256();
+            HttpClient client = new HttpClient();
+            string url = $"{URL}{URLENDPOINT}/VE/Venezuela/.json";
+            var response = client.GetStringAsync(url).Result;
+            Rootobject result = JsonConvert.DeserializeObject<Rootobject>(response);
+            return View(result.data.ad_list);
+        }
+        public IActionResult Pagination(int page)
+        {
+            HMACSHA256 hash = new HMACSHA256();
+            HttpClient client = new HttpClient();
+            string url = $"{URL}{URLENDPOINT}/VE/Venezuela/.json?page={page}";
+            var response = client.GetStringAsync(url).Result;
+            return View(response);
         }
 
         public IActionResult About()
---
+++ localb/Models/Ads.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace localb.Models
+{
+
+
+
+    public class Rootobject
+    {
+        public Pagination pagination { get; set; }
+        public Data data { get; set; }
+    }
+
+    public class Pagination
+    {
+        public string next { get; set; }
+        public string prev { get; set; }
+    }
+
+    public class Data
+    {
+        public Ad_List[] ad_list { get; set; }
+        public string ad_count { get; set; }
+    }
+
+    public class Ad_List
+    {
+        public data data { get; set; }
+        public Actions actions { get; set; }
+    }
+
+    public class data
+    {
+        public string visible { get; set; }
+        public string hidden_by_opening_hours { get; set; }
+        public string location_ { get; set; }
+        public string countrycode { get; set; }
+        public string city { get; set; }
+        public string trade_type { get; set; }
+        public string online_provider { get; set; }
+        public string first_time_limit_btc { get; set; }
+        public string volume_coefficient_btc { get; set; }
+        public string sms_verification_required { get; set; }
+        public string reference_type { get; set; }
+        public string display_reference { get; set; }
+        public string currency { get; set; }
+        public string lat { get; set; }
+        public string lon { get; set; }
+        public string min_amount { get; set; }
+        public string max_amount { get; set; }
+        public string max_amount_available { get; set; }
+        public string limit_to_fiat_amounts { get; set; }
+        public string ad_id { get; set; }
+        public string temp_price_usd { get; set; }
+        public string ing { get; set; }
+        public Profile profile { get; set; }
+        public string require_feedback_score { get; set; }
+        public string require_trade_volume { get; set; }
+        public string require_trusted_by_advertiser { get; set; }
+        public string payment_window_minutes { get; set; }
+        public string bank_name { get; set; }
+        public string track_max_amount { get; set; }
+        public string atm_model { get; set; }
+    }
+
+    public class Profile
+    {
+        public string username { get; set; }
+        public string name { get; set; }
+        public string last_online { get; set; }
+        public string trade_count { get; set; }
+        public string feedback_score { get; set; }
+    }
+
+    public class Actions
+    {
+        public string public_view { get; set; }
+    }
+
+}
+
